ea8da067e9724d19b70bfd1ee8aab6a6ddfb4542
GetoarNishefci <getoar.n1@gmail.com>
2025-09-02 13:20:05 +0200

    feat:added add card,dashboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,26 @@
-const HomePage = () =>{
+import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate= 0
+
+const HomePage = async() =>{
+
+    const products = await getProducts({isFeatured:true})
+    const billboard = await getBillboard("977a8c77-ae72-48d4-bb76-5911b14ff56f")
+
     return (
-            <div>Hello Store</div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                 <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products}/>
+            </div>
+        </Container>
+
     )
 }
 
