9a99106d1bf675249fd91d4189b2f3ac1022e229
GetoarNishefci <getoar.n1@gmail.com>
2025-09-24 16:50:31 +0200

    fix:billboard style

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,19 +8,22 @@ const Billboard:React.FC<BillboardProps> = ({
 }) =>{
 
     return(
-        <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-            <div style={{backgroundImage:`url(${data.imageUrl})`}}   className="rounded-xl relative h-[400px] w-full overflow-hidden bg-cover bg-center "
->
-                <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-                    <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-                        {data.label}
+<div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+  <div
+    style={{ backgroundImage: `url(${data.imageUrl})` }}
+    className="rounded-xl relative h-[400px] w-full overflow-hidden bg-cover bg-center"
+  >
+    <div className="absolute inset-0 bg-black/25"></div>
 
-                    </div>
+    <div className="relative h-full w-full flex flex-col justify-center items-center text-center gap-y-4">
+      <div className="font-poppins font-semibold text-2xl sm:text-3xl lg:text-4xl text-white drop-shadow-xl px-4 py-2 bg-white/10 rounded-md backdrop-blur-sm max-w-xs sm:max-w-md">
+        {data.label}
+      </div>
+    </div>
+  </div>
+</div>
 
-                </div>
-            </div>
 
-        </div>
     )
 }
 
